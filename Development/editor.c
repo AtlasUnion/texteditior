6b3ccbb68db0c1d551a2b357e6c7fcf118f024ba
Ryan <zruan@bu.edu>
2019-04-03 18:14:10 -0400

    Add welcome message; Implemet Cursor Movement

--- Development/editor.c
+++ Development/editor.c
@@ -13,9 +13,18 @@
 
 #define CTRL_KEY(k) ((k) & 0x1f) // mimic that ctrl strips bits 5 and 6
 #define ABUF_INIT {NULL,0}
+#define EDITOR_VERSION "0.0.1"
+
+enum editorKey {
+    ARROW_LEFT = 1000,
+    ARROW_RIGHT ,
+    ARROW_UP,
+    ARROW_DOWN
+};
 
 /*** data  ***/
 struct editorConfig {
+    int cx, cy;
     int screenRows;
     int screenCols;
     struct termios orig_termios;
@@ -132,13 +141,36 @@ void enableRawMode() {
  * 
  * @return char 
  */
-char editorReadKey() {
+int editorReadKey() {
     int nread;
     char c;
     while((nread = read(STDIN_FILENO, &c, 1)) != 1) {
         if (nread == -1 && errno != EAGAIN) die("read");
     }
-    return c;
+
+    /*
+    ** Capture arrow keys
+    ** Note arrow key return multiple bytes
+    */
+    if (c == '\x1b') {
+        char seq[3];
+
+        if (read(STDIN_FILENO, &seq[0], 1) != 1) return '\x1b';
+        if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
+
+        if (seq[0] == '[') {
+            switch (seq[1])
+            {
+                case 'A': return ARROW_UP;
+                case 'B': return ARROW_DOWN;
+                case 'C': return ARROW_RIGHT;
+                case 'D': return ARROW_LEFT;
+            }
+        }
+        return '\x1b';
+    } else {
+        return c;
+    }
 }
 
 /**
@@ -200,8 +232,31 @@ int getWindowSize(int* rows, int* cols) {
 void editorDrawRows(struct abuf* ab) {
     int y;
     for (y = 0; y < E.screenRows; y++) {
-        // write(STDOUT_FILENO, "~", 1);
-        abAppend(ab, "~", 1);
+        if (y == E.screenRows/3) {
+            char welcome[80];
+            int welcomelen = snprintf(welcome, sizeof(welcome), "Editor -- version %s", EDITOR_VERSION);
+
+            if (welcomelen > E.screenCols) welcomelen = E.screenCols;
+
+            /*
+            ** Center the welcome string
+            */
+            int padding = (E.screenCols - welcomelen)/2;
+            if (padding) {
+                abAppend(ab, "~", 1);
+                padding--;
+            }
+            while(padding--) abAppend(ab, " ", 1);
+            abAppend(ab, welcome, welcomelen);
+        } else {
+            // write(STDOUT_FILENO, "~", 1);
+            abAppend(ab, "~", 1);
+        }
+
+        /*
+        ** K -> Erase in line
+        */
+        abAppend(ab, "\x1b[K", 3);
         if (y < E.screenRows - 1) {
             // write(STDOUT_FILENO, "\r\n", 2);
             abAppend(ab, "\r\n", 2);
@@ -216,16 +271,26 @@ void editorRefreshScreen() {
     ** \x1b escape character, followed by [ to instruct terminal to execute certain commands
     ** 2J: J->Erase in Display, 2->Erase all of the display
     ** H-> Cursor Position, take two arguments: row and column number, default to 1
+    ** h, l -> ?25 hide/showing the cursor
     */
     // write(STDOUT_FILENO, "\x1b[2J", 4); 
     // write(STDOUT_FILENO, "\x1b[H", 3);
-    abAppend(&ab, "\x1b[2J", 4);
+    abAppend(&ab, "\x1b[?25l",6);
+    // abAppend(&ab, "\x1b[2J", 4);
     abAppend(&ab, "\x1b[H", 3);
 
     editorDrawRows(&ab);
 
+    /*
+    ** Move cursor
+    */
+    char buf[32];
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+    abAppend(&ab, buf, strlen(buf));
+
     // write(STDOUT_FILENO, "\x1b[H", 3);
-    abAppend(&ab, "\x1b[H", 3);
+    // abAppend(&ab, "\x1b[H", 3);
+    abAppend(&ab, "\x1b[?25h",6);
 
     write(STDOUT_FILENO, ab.b, ab.len);
     abFree(&ab);
@@ -233,8 +298,26 @@ void editorRefreshScreen() {
 
 /*** input ***/
 
+void editorMoveCursor(int key) {
+    switch (key)
+    {
+        case ARROW_LEFT:
+            E.cx--;
+            break;
+        case ARROW_RIGHT:
+            E.cx++;
+            break;
+        case ARROW_UP:
+            E.cy--;
+            break;
+        case ARROW_DOWN:
+            E.cy++;
+            break;
+    }
+}
+
 void editorProcessKeypress() {
-    char c = editorReadKey();
+    int c = editorReadKey();
 
     switch (c)
     {
@@ -243,12 +326,24 @@ void editorProcessKeypress() {
             write(STDOUT_FILENO, "\x1b[H", 3);
             exit(0);
             break;
+        case ARROW_UP:
+        case ARROW_DOWN:
+        case ARROW_LEFT:
+        case ARROW_RIGHT:
+            editorMoveCursor(c);
+            break;
     }
 }
 
 /*** init ***/
 
 void initEditor() {
+    /*
+    ** Set cursor position
+    */
+    E.cx = 0;
+    E.cy = 0;
+
     if (getWindowSize(&E.screenRows, &E.screenCols) == -1)
         die("getWindowSize");
 }
